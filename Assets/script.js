6b97669deb0a76dedfc813c1c7b8aba5dc16d0e5
Ricky <rickyjoet@gmail.com>
2021-05-15 13:58:52 -0700

    weather info on html

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,13 @@
-document.querySelector("#search-btn").addEventListener("click", function(event) {
-    console.log("click")
-    var query = "https://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&appid=c55fea575b010c6f9a308291c0d31def&"
-}) 
+function inputSearch () {
+    var city  = document.getElementById("searchbox").value;
+    console.log(city);
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&current.uvi&units=imperial&appid=c55fea575b010c6f9a308291c0d31def&`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    // api openweather app changed finding uvi 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&appid=c55fea575b010c6f9a308291c0d31def&')
-  .then(response => response.json())
-  .then(data => console.log(data));
+}
+
+
 
