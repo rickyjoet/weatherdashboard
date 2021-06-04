ca109622728cf4f996c00966491a45d624b67560
Ricky <rickyjoet@gmail.com>
2021-06-04 13:18:25 -0700

    added current temp

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,22 @@
 function inputSearch () {
     // var city  = document.getElementById("searchbox").value;
     var city  = document.querySelector('#searchbox').value;
+    var lat;  
+    var lon; 
+    var currentTemp;
+    var currentWind;
+    var currentHumidty;
+    var currentUvi;
     // console.log(city);
     
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&&appid=c55fea575b010c6f9a308291c0d31def&`)
     .then(response => response.json())
     .then(data => {
-        // console.log(data);
+        console.log(data);
+        // console.log(data.city.coord.lat);
+        // console.log(data.city.coord.lon);
+        lat= data.city.coord.lat;
+        lon= data.city.coord.lon;
         // console.log(data.list[2].main.temp);
         // console.log(data.list[0]);
         // console.log(data.list[10]);
@@ -16,8 +26,36 @@ function inputSearch () {
         
         // to do - use list [0] for todays temp
 
-        console.log(data.city.cord);
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=c55fea575b010c6f9a308291c0d31def&`)
+        .then(response => response.json())
+        .then(data => {console.log(data)
+        console.log(data.current.uvi);
+        console.log(data.current.humidity);
+        console.log(data.current.temp);
+        console.log(data.current.wind_speed);
 
+        currentTemp = data.current.temp;
+        currentWind = data.current.wind_speed;
+        currentHumidty = data.current.humidity;
+        currentUvi = data.current.uvi;
+        
+        console.log(currentTemp);
+        document.getElementById('temp').innerText = "temp: " + currentTemp + " ℉";
+        console.log(currentTemp);
+        document.getElementById('temp').innerText = "temp: " + currentTemp + " ℉";
+        console.log(currentTemp);
+        document.getElementById('temp').innerText = "temp: " + currentTemp + " ℉";
+        console.log(currentTemp);
+        document.getElementById('temp').innerText = "temp: " + currentTemp + " ℉";
+        console.log(currentTemp);
+        document.getElementById('temp').innerText = "temp: " + currentTemp + " ℉";
+
+    });
+        
+        
+        
+        
+        
         var weather = [data.list[0], data.list[2], data.list[10], data.list[18], data.list[26], data.list[34]]
         for (i= 0; i < weather.length; i++){
             // console.log(weather[i])
@@ -25,19 +63,18 @@ function inputSearch () {
             // console.log(date);
             date = new Date(date).toDateString();
             // console.log(date)
-
+            
             var temp = weather[i].main.temp;
-            // console.log(temp) 
-            // document.getElementById('temp').innerText = temp + " ℉";
-    
+           
+            
             var wind = weather[i].wind.speed;
             // console.log(wind);
             // document.getElementById('wind').innerText = wind + " mph";
-    
+            
             var humidity = weather[i].main.humidity;
             // console.log(humidity);
             // document.getElementById('humidity').innerText = humidity + "%";
-
+            
             if (i == 0) {
                 document.getElementById('day1').innerText = date + " temp: " + temp + " wind: " + wind + " mph " + " humidity: " + humidity + "%";  
             } else if (i==1) {
@@ -50,15 +87,19 @@ function inputSearch () {
                 document.getElementById('day5').innerText = date + " temp: " + temp + " wind: " + wind + " mph " + " humidity: " + humidity + "%";  
             }
         }
-
+        
         
         
         // api openweather app changed finding uvi
-
+        
+        
+        
     });
 
+  
 
 }
+ 
 
 
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
